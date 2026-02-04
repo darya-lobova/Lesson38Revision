83fe9b8f0237ac9f2880729178bc5adeb6ac1ffe
darya-lobova <dasha.lobova.it@gmail.com>
2026-02-04 17:48:00 +0300

    task 03 process

--- Task03/logic.cpp
+++ Task03/logic.cpp
@@ -2,7 +2,12 @@
 
 bool is_power_of_two(int number) {
 
-	
+	while (number % 2 == 0) {
+		int number1 = number / 2;
+		if (number1 / number % 2 == 0) {
+			return true;
+		}
+	}
 
-	return 0;
+	return false;
 }
