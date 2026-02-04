2a4e501fb462b581a680776b1cb748808148b4c4
darya-lobova <dasha.lobova.it@gmail.com>
2026-02-04 18:31:56 +0300

    task 05 final

---
+++ Task05/logic.cpp
@@ -0,0 +1,15 @@
+bool is_digits_count_even(long long number) {
+	number = number < 0 ? -number : number;
+
+	int count = 0;
+
+	while (number > 0) {
+		number /= 10;
+		count++;
+	}
+	if (count != 0 && count % 2 == 0) {
+		return true;
+	}
+
+	return false;
+}
---
+++ Task05/logic.h
@@ -0,0 +1,3 @@
+#include <iostream>
+using namespace std;
+bool is_digits_count_even(long long number);
