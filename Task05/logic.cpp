762bc2c6b7ee46b6017a6a3286846f8c96b2f289
darya-loboca <dasha.lobova.it@gmail.com>
2026-02-11 20:21:12 +0300

    Solved task 05

--- Task05/logic.cpp
+++ Task05/logic.cpp
@@ -1,15 +1,14 @@
+#include "logic.h"
+
 bool is_digits_count_even(long long number) {
 	number = number < 0 ? -number : number;
 
-	int count = 0;
+	int count = 1;
 
-	while (number > 0) {
+	while (number > 9) {
 		number /= 10;
 		count++;
 	}
-	if (count != 0 && count % 2 == 0) {
-		return true;
-	}
 
-	return false;
+	return count % 2 == 0;
 }
